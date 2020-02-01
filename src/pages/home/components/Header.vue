8ced473b15416ff17a8aeb5ca9501e0997de381f
董洋 <dongyang@dongyangdeMBP.lan>
2020-02-01 20:09:43 +0800

    add header

---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+    this is header
+    <span class="iconfont">&#xe658;</span>
+</div>
+</template>
+<script>
+export default {
+  name: 'HomeHeader'
+}
+</script>
+<style lang='stylus'>
+@import '~styles/varibles.styl'
+
+</style>
